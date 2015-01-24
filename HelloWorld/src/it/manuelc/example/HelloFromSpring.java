792ce90eb9ebd16da6ff8ee2e6861bd03a08ecc9
Manuel Ciofo <Manuel.ciofo@gmail.com>
2015-01-24 15:50:53 +0100

    Create HelloFromSpring.java

---
+++ HelloWorld/src/it/manuelc/example/HelloFromSpring.java
@@ -0,0 +1,16 @@
+package it.manuelc.example;
+
+public class HelloFromSpring {
+	
+	private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+		 
+	public void display()
+	{
+		System.out.println(message);
+	}
+	
+}
